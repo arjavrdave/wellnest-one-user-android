467ff6e188c804fa03df425f72256f493f7cdd24
WellAsohk <ashok.d@wellnest.tech>
2023-01-23 20:19:42 +0530

    searching issue resolve
    task/230

--- app/src/main/java/com/wellnest/one/ui/termofservice/TermsOfServiceActivity.kt
+++ app/src/main/java/com/wellnest/one/ui/termofservice/TermsOfServiceActivity.kt
@@ -1,20 +1,20 @@
 package com.wellnest.one.ui.termofservice
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.wellnest.one.R
 import com.wellnest.one.databinding.ActivityTermsOfServiceBinding
 
 class TermsOfServiceActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityTermsOfServiceBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_terms_of_service)
-        binding.pdfView.fromStream(resources.openRawResource(R.raw.wellnest_tos)).load()
-        binding.imgBack.setOnClickListener {
-            finish()
-        }
+
+        binding.pdfView.fromAsset("wellnest_tos.pdf").load()
+
     }
 }
